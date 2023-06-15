990792ec7b6e3d03433558b7cfc95f4352814046
chernenkoik <chernenkoik@tutu.tech>
2023-06-15 12:26:26 +0400

    chore: configure eslint-kit and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,10 @@
-module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+const { configure, presets } = require('eslint-kit')
+
+module.exports = configure({
+  presets: [
+    presets.react({ version: '18.0' }),
+    presets.effector(),
+    presets.prettier(),
+    presets.typescript(),
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+})
